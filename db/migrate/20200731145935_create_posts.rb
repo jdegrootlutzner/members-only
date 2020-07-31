c9b18519a1c6b78b4be2bf292e4cf79b495b765a
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2020-07-31 11:31:50 -0400

    set up devise and run migrations

--- db/migrate/20200731145935_create_posts.rb
+++ db/migrate/20200731145935_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.text :title
       t.text :body
-
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
